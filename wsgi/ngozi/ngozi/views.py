74477902ec003bef7a2b3c4f59194cbab5fe25ce
DMoney <yoshimitsu12@gmail.com>
2016-02-02 22:16:48 -0500

    Update template

--- wsgi/ngozi/ngozi/views.py
+++ wsgi/ngozi/ngozi/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'ngozi/index.html', {})
+    context = {
+        'title': 'Ngozi and Adonis',
+        'email': 'adonis@gmail.com'
+    }
+    return render(request, 'ngozi/index.html', context)
