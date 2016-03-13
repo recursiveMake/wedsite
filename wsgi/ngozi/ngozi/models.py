5e46233c0fd295be478ae3c18ec5eda2b4d9e5c6
adonis <yoshimitsu12@gmail.com>
2016-03-13 17:24:09 -0400

    Add help messages to form

--- wsgi/ngozi/ngozi/models.py
+++ wsgi/ngozi/ngozi/models.py
@@ -20,9 +20,9 @@ class GuestCount(object):
 
 
 class Rsvp(models.Model):
-    name = CharField(max_length=128)
-    email = EmailField()
+    name = CharField(max_length=128, verbose_name='Name(s)', help_text='Names of all guests RSVPing')
+    email = EmailField(help_text='An email address where we may contact you')
     adult_guests = CharField(max_length=1, choices=GuestCount.COUNTS, default=GuestCount.TWO, verbose_name='Adult Guests')
     child_guests = CharField(max_length=1, choices=GuestCount.COUNTS, default=GuestCount.NONE, verbose_name='Children')
     entered = DateTimeField(auto_now_add=True)
-    message = TextField(max_length=4096, blank=True, null=True)
+    message = TextField(max_length=4096, blank=True, null=True, help_text='Thoughts, kind words, and well wishes')
